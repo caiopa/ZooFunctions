6a441d48ac8bce49ea2d8ffe5d9f913bfe2d7d53
Caio Pereira Antunes <caioefg@gmail.com>
2022-04-14 16:41:03 -0300

    ex1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) return [];
+  const animais = species.filter((animal) => ids.includes(animal.id));
+  return animais;
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds(''));
