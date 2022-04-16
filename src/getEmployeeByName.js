b9cc4021c40b423e349488813ec639029218e8ae
Caio Pereira Antunes <caioefg@gmail.com>
2022-04-15 21:29:04 -0300

    arrumando

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,8 +2,9 @@ const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
+
   if (!employeeName) return {};
-  return employees.filter((obj) => employeeName === obj.firstName || employeeName === obj.lastName);
+  return employees.find((obj) => employeeName === obj.firstName || employeeName === obj.lastName);
 }
 
 module.exports = getEmployeeByName;
